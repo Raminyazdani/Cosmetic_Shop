0a013b2ae838f0aa97f1299fd8a131f1a8ea1944
ramin_yz <yazdani76ramin@gmail.com>
2022-12-22 19:30:15 +0330

    CosmeticShop | chore(Core.models): Create Base Models
    includes :  CoreModel,CoreModelUniversal

--- Core/models.py
+++ Core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class CoreModel(models.Model):
+
+    class Meta:
+        abstract = True
+
+class CoreModelUniversal(CoreModel):
+    time_created = models.DateTimeField
+    time_modified = models.DateTimeField
+    is_delete = models.BooleanField
+    is_active = models.BooleanField
+    class Meta:
+        abstract=True
