d9787e072de88271b5993415d0d77fa3358919b9
ramin_yz <yazdani76ramin@gmail.com>
2023-01-06 10:36:34 +0330

    CosmeticShop | chore(Branch-feature-Products,scope-Users): Modify Admin Models | Modify admin models for : User

--- Users/admin.py
+++ Users/admin.py
@@ -1,21 +1,42 @@
 from django.contrib import admin
 from .models import User
-from Core.admin import BaseAdmin
+from Core.admin import BaseAdmin, BaseAdminSlugUser
+
 
 # Register your models here.
 
 
 @admin.register(User)
-class UserAdmin(BaseAdmin):
-    list_display = ['id', 'phone_number', 'is_staff', 'is_superuser', 'is_admin', 'is_verified', 'is_delete',
-                    'is_market', 'is_costumer', 'is_active', 'created_at']
-    list_filter = ['is_staff', 'is_superuser', 'is_active', 'is_costumer', 'is_market', "is_delete", "is_verified",
-                   "is_active"]
+class UserAdmin(BaseAdminSlugUser):
+    list_display = ['id', 'phone_number', 'is_costumer', 'is_market', 'is_staff', 'is_active', 'is_admin',
+                    'is_superuser', 'is_verified', 'slug', 'is_delete', 'created_at', 'modified_at']
+    list_filter = ['is_costumer', 'is_market', 'is_staff', 'is_active', 'is_admin',
+                   'is_superuser', 'is_verified', 'is_delete', 'created_at']
+    list_editable = ['is_costumer', 'is_market', 'is_staff', 'is_active', 'is_admin',
+                     'is_superuser', 'is_verified', 'is_delete']
+
     search_fields = ['phone_number']
     ordering = ['phone_number']
     filter_horizontal = []
-    fieldsets = []
+    fieldsets = ((
+                     "Profiling", {
+                         'fields': (('phone_number', 'slug'), ('is_costumer', 'is_market')),
+
+                         'classes': ('extrapretty')
+                     }),
+                 ("Conditions", {
+                     'fields': (('is_staff', 'is_active', 'is_admin',
+                                 'is_superuser', 'is_verified',
+                                 'is_delete'),),
+                     'classes': ('extrapretty')
+                 }),
+                 ("Time", {
+                     'fields': (('created_at', 'modified_at'),),
+                     'classes': ('extrapretty')
+                 })
+    )
     add_fieldsets = []
     readonly_fields = ['created_at', 'modified_at']
     list_per_page = 25
     list_max_show_all = 100
+    prepopulated_fields = {"slug": ("phone_number",)}
