21d5ecea66aeb893d5134559276c496a07d4ec4a
ramin_yz <yazdani76ramin@gmail.com>
2023-01-06 10:42:11 +0330

    CosmeticShop | chore(Branch-feature-Products,scope-Core): Modifying BaseModels In Models |

--- Core/models.py
+++ Core/models.py
@@ -1,24 +1,33 @@
 from django.db import models
-from django.utils.translation import gettext_lazy as _
-
-
-# Create your models here.
 from Core.managers import BaseManager
+from Core.fields import ProjectField
+from django.db import models
 
 
 class CoreModel(models.Model):
+    """
+    Core Model
+
+    """
+    id = ProjectField.CustomIdField()
+
     class Meta:
         abstract = True
-
 
 
 class CoreModelUniversal(CoreModel):
+    """
+    Core Model Universal with universal fields and managers
+    """
+    is_delete = ProjectField.CustomIsDeletedField()
+    created_at = ProjectField.CustomCreatedAtField()
+    modified_at = ProjectField.CustomModifiedAtField()
 
-    is_delete = models.BooleanField(default=False, verbose_name=_('Is Delete'), help_text=_('Is this user deleted?'))
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name=_('Created At'), help_text=_('Created At'))
-    modified_at = models.DateTimeField(auto_now=True, verbose_name=_('Modified At'), help_text=_('Modified At'))
+    objects = BaseManager()
+    subset = BaseManager()
 
     class Meta:
         abstract = True
+
 
 
