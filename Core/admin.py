3d03303d78e18f9510a328a6177a0decb9b7f32f
ramin_yz <yazdani76ramin@gmail.com>
2023-01-06 10:34:45 +0330

    CosmeticShop | chore(Branch-feature-Products,scope-Core): Adding BaseAdmin Models | These Models will be modified in future

--- Core/admin.py
+++ Core/admin.py
@@ -1,15 +1,21 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.forms import models, BaseModelFormSet
+
 
 @admin.action(description='Soft Delete selected objects')
-def soft_delete_users(modeladmin, request, queryset):
+def soft_delete(modeladmin, request, queryset):
     queryset.update(is_delete=True)
 
 
-class BaseAdmin(admin.ModelAdmin):
+@admin.action(description='Restore selected objects')
+def Restore_delete(modeladmin, request, queryset):
+    queryset.update(is_delete=False)
 
-    actions = [soft_delete_users]
+
+class BaseAdmin(admin.ModelAdmin):
+    actions = [soft_delete, Restore_delete]
 
     def get_queryset(self, request):
         # use our manager, rather than the default one
@@ -19,4 +25,48 @@ class BaseAdmin(admin.ModelAdmin):
         ordering = self.ordering or ()  # otherwise we might try to *None, which is bad ;)
         if ordering:
             qs = qs.order_by(*ordering)
+
         return qs
+
+
+class BaseAdminSlug(BaseAdmin):
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            self.prepopulated_fields = {}
+            return ['slug'] + self.readonly_fields
+        return self.readonly_fields
+
+    class Media:
+        js = ("/statics/Base/Admin_statics/Admin_slug.js", "/statics/Base/Admin_statics/jquery.js")
+
+
+class BaseAdminSlugUser(BaseAdminSlug):
+    class Media:
+        js = ("/statics/Base/Admin_statics/Admin_user_slug.js", "/statics/Base/Admin_statics/jquery.js")
+
+
+class BaseFormSetInlineNoDelete(models.BaseInlineFormSet):
+    def __init__(self, *args, **kwargs):
+        super(BaseFormSetInlineNoDelete, self).__init__(*args, **kwargs)
+        self.can_delete = False
+
+
+class CustomInlineAdmin(admin.TabularInline):
+    classes = ('collapse',)
+
+    def get_extra(self, request, obj=None, **kwargs):
+        extra = 1
+        return extra
+
+
+class CustomInlineAdminOneToMany(CustomInlineAdmin):
+    formset = BaseFormSetInlineNoDelete
+
+
+class CustomStackedAdmin(admin.StackedInline):
+    classes = ('collapse',)
+
+    def get_extra(self, request, obj=None, **kwargs):
+        extra = 1
+        return extra
