a872bea32a19ff240a4f6861314e2de0f120aa91
ramin_yz <yazdani76ramin@gmail.com>
2023-01-08 22:04:42 +0330

    CosmeticShop | chore(Branch-feature-Products,scope-Core):Modify Admin Base Slug Js

--- statics/Base/Admin_statics/Admin_category_slug.js
+++ statics/Base/Admin_statics/Admin_category_slug.js
@@ -1,8 +1,11 @@
-
 window.onload = function () {
     var input_var = document.querySelectorAll('div.form-row.field-name>div>input')[0];
     var output_var = document.querySelectorAll('div.form-row.field-slug>div>div.readonly')[0];
     var output_add = document.querySelector("#id_slug")
+
+    var parent_var_select = document.querySelector("#id_parent")
+    console.log(parent_var_select)
+
     var extra_fieldsets = document.querySelectorAll("div > fieldset.collapse")
     for (let i = 0; i < extra_fieldsets.length; i++) {
         let text_area = extra_fieldsets[i].querySelectorAll("textarea.vLargeTextField")
@@ -17,8 +20,17 @@ window.onload = function () {
 
     if (input_var && output_var) {
         input_var.addEventListener("input", function () {
-            output_var.innerHTML = string_to_slug(this.value);
+            output_var.innerHTML = parent_var.text() + "/" + string_to_slug(this.value);
         })
+        parent_var_select.addEventListener("change", function () {
+            parent_var = $("#id_parent option:selected")
+            if (parent_var.text() == "---------") {
+                output_var.innerHTML = string_to_slug(input_var.value);
+            } else {
+                output_var.innerHTML = parent_var.text() + "/" + string_to_slug(input_var.value);
+            }
+        })
+
     }
     if (input_var && output_add) {
         output_add.setAttribute("readonly", "readonly");
