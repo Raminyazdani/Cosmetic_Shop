074b02c4026e2100d2bb7d99294f12066f65cb5f
ramin_yz <yazdani76ramin@gmail.com>
2023-01-08 05:01:30 +0330

    CosmeticShop | chore(Branch-feature-Products,scope-Core):Change in ProjectUtils.py

--- Core/utils/ProjectUtils.py
+++ Core/utils/ProjectUtils.py
@@ -23,10 +23,61 @@ class CustomValidators:
                                  MaxLengthValidator(100, message=_('Short description must be at most 100 characters'))]
     DescriptionValidator = [MinLengthValidator(10, message=_('Description must be at least 10 characters')),
                             MaxLengthValidator(1000, message=_('Description must be at most 1000 characters'))]
-    PriceValidator = [MinValueValidator(0, message=_('Price must be at least 0')),
-                      MaxValueValidator(1000000000, message=_('Price must be at most 1000000000'))]
+    DollarPriceValidator = [MinValueValidator(0, message=_('Price must be at least 0')),
+                            MaxValueValidator(1000000000, message=_('Price must be at most 1000000000'))]
     TitleValidator = [
         RegexValidator(regex=CustomRegex.title_regex,
                        message=_('Title must be a valid string with 3 to 20 characters'))]
     BodyValidator = [MinLengthValidator(10, message=_('Body must be at least 10 characters')),
                      MaxLengthValidator(250, message=_('Body must be at most 250 characters'))]
+
+class CustomStringMaker:
+    class ForeignKey:
+        @staticmethod
+        def to_gen(app_name,class_model):
+            return app_name+'.'+class_model
+        @staticmethod
+        def related_name_gen(class_name,related_name_default):
+            return class_name.lower()+'_'+related_name_default.lower()
+
+    class ManyToMany:
+        @staticmethod
+        def to_gen(app_name,class_model):
+            return app_name+'.'+class_model
+
+        @staticmethod
+        def related_name_gen(class_name,related_name_default):
+            return class_name.lower()+'_'+related_name_default.lower()
+
+        @staticmethod
+        def through_gen(class_name,app_model_name,through_fields=None):
+            #       "class_name" :                  "Product",              "Category"
+            #       "field_name":                   "Category",             "Product #
+            #       "app_name_destination":         "Products",             "Products"
+            #       "app_name_model_destination":   "Category",             "Product"
+            #       "through_fields":               [None, "category_id"]   ["product_id",None]
+            #       "through":                      "ProductCategory",      "ProductCategory"
+
+
+            if through_fields is None:
+                return class_name.capitalize()+app_model_name.capitalize()
+            else:
+                if through_fields[0] is None:
+                    return class_name.capitalize()+app_model_name.capitalize()
+                elif through_fields[1] is None:
+                    return app_model_name.capitalize()+class_name.capitalize()
+
+
+
+        @staticmethod
+        def through_fields_gen(class_name,app_model_name,through_fields=None):
+            #        "through_fields": ["product_id", None],
+            if through_fields is None:
+                result = [class_name.lower(), app_model_name.lower()]
+            else:
+                if through_fields[0] is None:
+
+                    result  = [class_name.lower()+"_id",through_fields[1]]
+                elif through_fields[1] is None:
+                    result  = [class_name.lower()+"_id",through_fields[0],]
+            return result
