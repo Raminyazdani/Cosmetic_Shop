cd77c92c36adb1b2950f7b213573598ba65e250e
ramin_yz <yazdani76ramin@gmail.com>
2023-01-11 23:01:22 +0330

    test-1

--- Core/management/commands/ManagerUtils/ModelForeigns_maker.py
+++ Core/management/commands/ManagerUtils/ModelForeigns_maker.py
@@ -1,14 +1,25 @@
+from Core.management.commands.ManagerUtils.CheckLines import check_lines
+from Core.management.commands.ManagerUtils.RemoveEmptyLines import remove_empty_lines
+
 def ModelForeigns_maker(File, Models, App_name, Scope_parent, Option):
-    result = []
     header = f"""
 from django.utils.functional import cached_property
 
 from Core.utils.ProjectUtils import GetNameSpaceProperty
 
 """
+    result = []
+    remove_empty_lines(File)
 
-    with open(File, "w") as f:
-        f.write(header)
+    if Option == "hard":
+        mode_file = "w"
+    else:
+        mode_file = "a"
+        Models = check_lines(File, Models, App_name, Scope_parent, header, prefix= "")
+
+    with open(File, mode_file) as f:
+        if mode_file== "w" :
+            f.write(header)
         for Model in Models:
             body = f"""
 
@@ -23,15 +34,15 @@ class {Model.capitalize()}:
     @cached_property
     def {Model.lower()}_count(self,teststring="{Model.lower()}",scopeparent="{Scope_parent.lower()}"):
 
-        return GetNameSpaceProperty.count(self, "{Model.lower()}","{Scope_parent.lower()}")
+        return GetNameSpaceProperty.count(self, teststring,scopeparent)
 
     @cached_property
     def {Model.lower()}_name(self,teststring="{Model.lower()}",scopeparent="{Scope_parent.lower()}"):
-        return GetNameSpaceProperty.name(self, "{Model.lower()}","{Scope_parent.lower()}")
+        return GetNameSpaceProperty.name(self, teststring,scopeparent)
 
 
 """
 
             f.write(body)
-            result.append(f"{App_name:10}.{Model.capitalize():>25}\t {'ModelForeigns':25} \t\tadded in ModelForeigns.py")
+            result.append(f"{App_name:10}.{Model.capitalize():<25}\t {'ModelForeigns':25} \t\tadded in ModelForeigns.py")
     return result
--- Core/management/commands/ManagerUtils/models_maker.py
+++ Core/management/commands/ManagerUtils/models_maker.py
@@ -1,25 +1,33 @@
+from Core.management.commands.ManagerUtils.CheckLines import check_lines
+from Core.management.commands.ManagerUtils.RemoveEmptyLines import remove_empty_lines
+
 def models_maker(File, Models, App_name, Scope_parent, Option):
-    result = []
     header = f"""
 from django.utils.translation import gettext_lazy as _
 
-from Core.ProjectMixins.Apps.{App_name.capitalize()}_Mixins import Model.capitalize()RequiredProperties
-# Create your models here.
+from Core.ProjectMixins.Apps.{App_name.capitalize()}_Mixins import ModelRequiredProperties
 from Core.fields import ProjectFields
 
 from Core.fields import ProjectFields
-from Core.models import CoreModel.capitalize()Universal
-# Core import
-# Create your models here.
+from Core.models import CoreModelUniversal
+
 
 """
+    result = []
+    remove_empty_lines(File)
 
-    with open(File, "w") as f:
-        f.write(header)
+    if Option == "hard":
+        mode_file = "w"
+    else:
+        mode_file = "a"
+        Models = check_lines(File, Models, App_name, Scope_parent, header, prefix = "")
+
+    with open(File, mode_file) as f:
+        if mode_file == "w":
+            f.write(header)
         for Model in Models:
             body = f"""
-
-class {Model.capitalize()}(Model.capitalize()RequiredProperties.{Model.capitalize()}Mixin,CoreModel.capitalize()Universal ):
+class {Model.capitalize()}(ModelRequiredProperties.{Model.capitalize()}Mixin,CoreModelUniversal ):
     \"\"\"
     Product Model.capitalize()
     \"\"\"
@@ -35,5 +43,5 @@ class {Model.capitalize()}(Model.capitalize()RequiredProperties.{Model.capitaliz
 """
 
             f.write(body)
-            result.append(f"{App_name:10}.{Model.capitalize():>25}\t {'Model':25} \t\tadded in models.py")
+            result.append(f"{App_name:10}.{Model.capitalize():<25}\t {'Model':25} \t\tadded in models.py")
     return result
