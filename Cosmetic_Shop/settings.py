e6c8ded6a895554423f40030bfafea58046b574a
ramin_yz <yazdani76ramin@gmail.com>
2023-01-11 07:12:43 +0330

    CosmeticShop | chore(Branch-feature-Shop,scope-Cosmetic_shop):Change in Installed Apps | Removing shop

--- Cosmetic_Shop/settings.py
+++ Cosmetic_Shop/settings.py
@@ -28,53 +28,25 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-DJANGO_INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-]
-PROJECT_INSTALLED_APPS = [
-    'APIs',
-    'Costumers',
-    'Markets',
-    'Products',
-    'Shop',
-    'Users',
-    'Core', ]
+DJANGO_INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'django.contrib.contenttypes', 'django.contrib.sessions', 'django.contrib.messages', 'django.contrib.staticfiles', ]
+PROJECT_INSTALLED_APPS = ['APIs', 'Costumers', 'Markets', 'Products',
+                          # 'Shops',
+                          'Users', 'Core', ]
 
 INSTALLED_APPS = [] + DJANGO_INSTALLED_APPS + PROJECT_INSTALLED_APPS
 
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
+MIDDLEWARE = ['django.middleware.security.SecurityMiddleware', 'django.contrib.sessions.middleware.SessionMiddleware', 'django.middleware.common.CommonMiddleware', 'django.middleware.csrf.CsrfViewMiddleware', 'django.contrib.auth.middleware.AuthenticationMiddleware', 'django.contrib.messages.middleware.MessageMiddleware', 'django.middleware.clickjacking.XFrameOptionsMiddleware', ]
 
 ROOT_URLCONF = 'Cosmetic_Shop.urls'
 
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
+TEMPLATES = [{
+    'BACKEND': 'django.template.backends.django.DjangoTemplates',
+    'DIRS': [BASE_DIR / 'templates'],
+    'APP_DIRS': True,
+    'OPTIONS': {
+        'context_processors': ['django.template.context_processors.debug', 'django.template.context_processors.request', 'django.contrib.auth.context_processors.auth', 'django.contrib.messages.context_processors.messages', ],
         },
-    },
-]
+    }, ]
 
 WSGI_APPLICATION = 'Cosmetic_Shop.wsgi.application'
 
@@ -85,26 +57,21 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
+AUTH_PASSWORD_VALIDATORS = [{
+    'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    }, {
+    'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    }, {
+    'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    }, {
+    'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    }, ]
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
@@ -122,9 +89,7 @@ USE_TZ = True
 
 STATIC_URL = 'statics/'
 TEMPLATES_URL = 'templates/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'statics/'
-]
+STATICFILES_DIRS = [BASE_DIR / 'statics/']
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
